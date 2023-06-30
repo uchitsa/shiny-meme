8b714293fc5f3a51bd2cf9bf85edff8d998b901a
uchitsa <uchitsa@ya.ru>
2023-07-01 02:26:26 +0300

    add fn push

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,3 +1,6 @@
+use std::mem;
+
+
 pub struct List {
     head: Link,
 }
@@ -15,5 +18,14 @@ struct Node {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
     }
 }
