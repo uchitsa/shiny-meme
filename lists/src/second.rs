225ed1b02db85ed88546f6aaf52965757cdfdbf8
uchitsa <uchitsa@ya.ru>
2023-07-11 02:02:55 +0300

    add peek method

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -29,6 +29,12 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&mut self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
